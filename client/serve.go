922b656429a74cada481c9b1728632cebec04e4a
Scott A. Matthews <scott@designsbysm.com>
2021-10-15 19:04:29 -0700

    bugfix client handler
    unknown /api/ request are passed to handler, added prefix check to return 404

--- client/serve.go
+++ client/serve.go
@@ -1,13 +1,20 @@
 package client
 
 import (
+	"net/http"
 	"net/http/httputil"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
 
 func serve(proxy *httputil.ReverseProxy) gin.HandlerFunc {
 	return func(c *gin.Context) {
+		if strings.HasPrefix(c.Request.URL.String(), "/api/") {
+			c.Status(http.StatusNotFound)
+			return
+		}
+
 		proxy.ServeHTTP(c.Writer, c.Request)
 	}
 }
