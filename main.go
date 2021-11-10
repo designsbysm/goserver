69be14d033f21cbd475ce58008167c48bebede05
Scott A. Matthews <scott@designsbysm.com>
2021-11-09 20:11:42 -0700

    remove rpc package

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ package main
 import (
 	"github.com/designsbysm/server-go/api"
 	"github.com/designsbysm/server-go/database"
-	"github.com/designsbysm/server-go/rpc"
 	"github.com/designsbysm/timber/v2"
 	"golang.org/x/sync/errgroup"
 )
@@ -26,7 +25,7 @@ func main() {
 	eg := new(errgroup.Group)
 
 	eg.Go(func() error { return api.Serve() })
-	eg.Go(func() error { return rpc.Serve() })
+	// eg.Go(func() error { return rpc.Serve() })
 
 	err := eg.Wait()
 	if err != nil {
